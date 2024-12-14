c3318843e0bba3bccd2929d025a9376607378d25
sshnote <springssh0504@naver.com>
2024-12-14 14:52:11 +0900

    first commit

---
+++ src/test/java/com/pp/PpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PpApplication.class)
+class PpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
